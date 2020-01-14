0400d5f2b72b1c7524e2c8698d852370ed8f7120
Daniel Vogel <daniel.vogel@rib-cosinus.com>
2020-01-14 17:55:46 +0100

    1.0.0.0

---
+++ WordSearch/Search/SearchResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearch.Search
+{
+    class SearchResult
+    {
+        [DisplayName("File Path")]
+        public string FilePath { get; set; }
+        [DisplayName("Text")]
+        public string Text { get; set; }
+    }
+}
